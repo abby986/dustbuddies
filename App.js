0672aa46d5edb4fc416bddbca71a943a3d1cfa08
OpherPhilms <opherlikespie@gmail.com>
2026-02-04 13:19:06 -0500

    Consolidated the App Navigation and App Navigator

--- App.js
+++ App.js
@@ -2,18 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './navigation/AppNavigation';
 import AppNavigator from './navigation/AppNavigator';
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <AppNavigator />
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
-}
+
 
 export default function App() {
   return (
     <>
+      <AppNavigator />
       <AppNavigation />
       <StatusBar style="auto" />
     </>
