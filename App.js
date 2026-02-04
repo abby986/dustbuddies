36cb78f57fbc174454dfafd8330dd2d7de7bee4d
snorm03 <sa519315@ucf.edu>
2026-02-04 12:38:46 -0500

    Profile and Bulletin Bodard screens updated

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import NavBar from './navigation/NavBar';
+import AppNavigation from './navigation/AppNavigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NavBar />
+    <>
+      <AppNavigation />
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </>
   );
 }
+
