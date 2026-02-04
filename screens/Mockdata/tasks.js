0e476837be6c482bb726f439b0a50e44c9f70aec
snorm03 <sa519315@ucf.edu>
2026-02-04 14:23:58 -0500

    Message screens fixed

--- screens/Mockdata/tasks.js
+++
@@ -1,5 +0,0 @@
-export const tasks = [
-    { id: '1', title: '1. Do the dishes', completed: false },
-    { id: '2', title: '2. Mop floors', completed: false },
-    { id: '3', title: '3. Take out trash', completed: true },
-];
