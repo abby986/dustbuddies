dece5c2bef1b6e67a99bcbda73754c6bef0fe696
Abigail Trebon <ab910876@ucf.edu>
2026-02-05 18:55:09 -0500

    Tasks screen updated with back button and profile, nav bar updated with proper style, fixed issue with profile access from buddy closet

--- navigation/NavBar.js
+++ navigation/NavBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//import image
+import { Image, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import BulletinBoardScreen from '../screens/BulletinBoardComponents/BulletinBoardScreen';
@@ -7,23 +9,89 @@ import HomeScreen from '../screens/HomeScreen';
 import MessagesScreen from '../screens/MessagesScreen';
 import ProfileScreen from '../screens/ProfileScreenComponents/ProfileScreen';
 
+//asset image imports
+import profileIcon from '../assets/images/bunny-head-nav-white.png';
+//icon imports
+// Vector icons
+import { Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
     <Tab.Navigator
-      initialRouteName='Home'
+      initialRouteName="Home"
       screenOptions={{
-        tabBarActiveTintColor: '#556DC2',
-        tabBarInactiveTintColor: 'gray',
         headerShown: true,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: '#556DC2',
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 10,
+
+        },
       }}
     >
-      <Tab.Screen name="Bulletin Board" component={BulletinBoardScreen} />
-      <Tab.Screen name="Tasks" component={TasksScreen} />
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Messages" component={MessagesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+
+      <Tab.Screen name="Bulletin Board" component={BulletinBoardScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <FontAwesome
+            name="sticky-note-o"
+            size={30}
+            color="#ffffff"
+
+          />
+        ),
+      }}
+      />
+      <Tab.Screen name="Tasks" component={TasksScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <MaterialCommunityIcons
+            name="broom"
+            size={32}
+            color="#ffffff"
+          />
+        ),
+      }}
+      />
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <Feather
+            name="home"
+            size={32}
+            color="#ffffff"
+          />
+        ),
+      }}
+      />
+      <Tab.Screen name="Messages" component={MessagesScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <Feather
+            name="message-circle"
+            size={32}
+            color="#ffffff"
+          />
+        ),
+      }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <Image
+            source={profileIcon}
+            style={{
+              width: 34,
+              height: 34,
+              tintColor: '#ffffff',
+            }}
+          />
+        ),
+      }}
+      />
     </Tab.Navigator>
   );
 }
--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, StyleSheet, TouchableOpacity, Image, Text, } from 'react-native';
 //import components from their respective files
 import TaskFilter from './TaskComponents/TaskFilter';
 import TaskItems from './TaskComponents/TaskItems';
 import TaskModal from './TaskComponents/TaskModal';
 import SubmitTask from './TaskComponents/SubmitTask';
 import VoteTask from './TaskComponents/VoteTask';
+//import icons
+import greenIcon from '../assets/images/green-bunny-profile.png';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function TasksScreen() {
+export default function TasksScreen({ navigation }) {
   const [showMyTasks, setShowMyTasks] = useState(true);
   const [selectedTask, setSelectedTask] = useState(null);
   const [activeView, setActiveView] = useState('LIST'); // LIST | MODAL | SUBMIT | VOTE
@@ -26,39 +29,59 @@ export default function TasksScreen() {
     setSelectedTask(task);
     task.mine ? setActiveView('MODAL') : setActiveView('VOTE');
   };
-
-  //if statements for  submit and vote
-  if (activeView === 'SUBMIT') {
-    return (
-      <SubmitTask
-        task={selectedTask}
-        onSubmit={() => setActiveView('LIST')}
-        onBack={() => setActiveView('LIST')}
-      />
-    );
-  }
-
-  if (activeView === 'VOTE') {
-    return (
-      <VoteTask
-        task={selectedTask}
-        onVote={() => setActiveView('LIST')}
-        onBack={() => setActiveView('LIST')}
-      />
-    );
-  }
+  //addition for profile and back button
 
   return (
-    <View>
-      <TaskFilter showMyTasks={showMyTasks} setShowMyTasks={setShowMyTasks} />
+    <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
 
-      <FlatList
-        data={showMyTasks ? myTasks : allTasks}
-        keyExtractor={(item) => item.key}
-        renderItem={({ item, index }) => (
-          <TaskItems task={item} number={index + 1} onPress={() => handleTaskPress(item)} />
-        )}
-      />
+      {/*new header*/}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.back}>‹</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Profile')}
+          activeOpacity={0.7}
+        >
+          <Image source={greenIcon} style={styles.profileIcon} />
+        </TouchableOpacity>
+      </View>
+
+      {/*refactor content*/}
+      {activeView === 'SUBMIT' && (
+        <SubmitTask
+          task={selectedTask}
+          onSubmit={() => setActiveView('LIST')}
+          onBack={() => setActiveView('LIST')}
+        />
+      )}
+
+      {activeView === 'VOTE' && (
+        <VoteTask
+          task={selectedTask}
+          onVote={() => setActiveView('LIST')}
+          onBack={() => setActiveView('LIST')}
+        />
+      )}
+
+      {activeView === 'LIST' && (
+        <>
+          <TaskFilter showMyTasks={showMyTasks} setShowMyTasks={setShowMyTasks} />
+
+          <FlatList
+            data={showMyTasks ? myTasks : allTasks}
+            keyExtractor={(item) => item.key}
+            renderItem={({ item, index }) => (
+              <TaskItems
+                task={item}
+                number={index + 1}
+                onPress={() => handleTaskPress(item)}
+              />
+            )}
+          />
+        </>
+      )}
 
       <TaskModal
         visible={activeView === 'MODAL'}
@@ -69,3 +92,28 @@ export default function TasksScreen() {
     </View>
   );
 }
+
+
+//styles
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+
+  back: {
+    fontSize: 40,
+    fontWeight: '300',
+  },
+
+  profileIcon: {
+    width: 48,
+    height: 48,
+    resizeMode: 'contain',
+  },
+});
