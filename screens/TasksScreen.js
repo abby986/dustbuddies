0828697a23b7f90c8bf59de1cd38ca16c977594b
Abigail Trebon <ab910876@ucf.edu>
2026-03-11 12:59:20 -0400

    updates to task screen to implement rotation logic

--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, FlatList, StyleSheet, TouchableOpacity, Image, Text, } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, StyleSheet, TouchableOpacity, Image, Text, ScrollView } from 'react-native';
 //import components from their respective files
 import TaskFilter from './TaskComponents/TaskFilter';
 import TaskItems from './TaskComponents/TaskItems';
@@ -9,13 +9,17 @@ import VoteTask from './TaskComponents/VoteTask';
 //import icons
 import greenIcon from '../assets/images/green-bunny-profile.png';
 import { Ionicons } from '@expo/vector-icons';
+//firebase imports
+import { db, auth } from "../firebase";
+import { collection, onSnapshot, addDoc, getDocs, query, where, orderBy, limit, serverTimestamp, doc, getDoc } from "firebase/firestore";
+
 
 export default function TasksScreen({ navigation }) {
   const [showMyTasks, setShowMyTasks] = useState(true);
   const [selectedTask, setSelectedTask] = useState(null);
   const [activeView, setActiveView] = useState('LIST'); // LIST | MODAL | SUBMIT | VOTE
 
-  //task array
+  /*removing hard coded task array, replacing with firestore data
   const allTasks = [
     { key: 'Do the dishes', mine: true },
     { key: 'Take out the trash', mine: false },
@@ -23,8 +27,151 @@ export default function TasksScreen({ navigation }) {
     { key: 'Water plants', mine: false },
   ];
 
-  const myTasks = allTasks.filter(task => task.mine);
+  const myTasks = allTasks.filter(task => task.mine); */
 
+  const [allTasks, setAllTasks] = useState([]);
+  const [presetTasks, setPresetTasks] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const userId = auth.currentUser?.uid;
+
+  //load the preset tasks for the dropdown menu
+  useEffect(() => {
+    async function loadPreset() {
+      const snap = await getDocs(collection(db, 'presetTasks'));
+      setPresetTasks(snap.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    }
+    loadPreset();
+  }, []);
+
+  //load the tasks from firestore
+  useEffect(() => {
+    if (!userId) return;
+    const q = query(collection(db, 'tasks'), orderBy('date', 'desc'));
+
+    const unsub = onSnapshot(q, (snap) => {
+      const list = snap.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          key: data.task,
+          mine: data.userId === userId,
+          raw: data,
+        };
+      });
+      setAllTasks(list);
+    });
+    return () => unsub();
+  }, [userId]);
+
+  const myTasks = allTasks.filter(t => t.mine);
+
+  // set up task rotation
+  function getNextTask(presetTasks, lastTaskName) {
+    if (!lastTaskName) {
+      return presetTasks[0].name;
+    }
+
+    const index = presetTasks.findIndex(t => t.name === lastTaskName);
+    const nextIndex = (index + 1) % presetTasks.length;
+
+    return presetTasks[nextIndex].name;
+  }
+
+  // assiging tassks in rotation
+  async function assignNextTask(selectedTaskName) {
+    console.log("assignNextTask CALLED", selectedTaskName);
+    if (!userId) {
+      console.log("NO USER ID");
+      return;
+    }
+
+    // get group id
+    const userRef = doc(db, "users", userId);
+    const userSnap = await getDoc(userRef);
+    console.log("userSnap exists:", userSnap.exists());
+    const groupId = userSnap.data()?.groupId;
+
+    if (!groupId) {
+      console.log("User has no groupId");
+      return;
+    }
+
+    // get group memebers
+    const groupRef = doc(db, "groups", groupId);
+    const groupSnap = await getDoc(groupRef);
+    console.log("groupSnap exists:", groupSnap.exists());
+    const memberIds = groupSnap.data()?.memberIds || [];
+    console.log("memberIds:", memberIds);
+
+
+    if (memberIds.length === 0) {
+      console.log("Group has no members");
+      return;
+    }
+
+    // get last task for rotation
+    const q = query(
+
+      collection(db, "tasks"),
+      where("groupId", "==", groupId),
+      orderBy("date", "desc"),
+      limit(1)
+    );
+
+    const snap = await getDocs(q);
+
+    console.log("snap size:", snap.size);
+    console.log("snap empty:", snap.empty);
+
+
+    let nextUserId;
+
+    if (snap.empty) {
+      // start with the first memeber to get tasks
+      nextUserId = memberIds[0];
+    } else {
+      const lastUserId = snap.docs[0].data().userId;
+      const lastIndex = memberIds.indexOf(lastUserId);
+      const nextIndex = (lastIndex + 1) % memberIds.length;
+      nextUserId = memberIds[nextIndex];
+    }
+
+    // get the next task from the preset task list
+    const lastTask = snap.empty ? null : snap.docs[0].data().task;
+    const nextTask = selectedTaskName;
+
+    // save to firestore
+    await addDoc(collection(db, "tasks"), {
+      userId: nextUserId,
+      groupId: groupId,
+      task: nextTask,
+      date: serverTimestamp(),
+    });
+
+    setShowDropdown(false);
+  }
+
+
+  //selected tasks will be saved to firestore
+  /*async function handleSelectTask(taskName) {
+    if (!userId) return;
+
+    await addDoc(collection(db, 'tasks'), {
+      userId,
+      task: taskName,
+      date: serverTimestamp(),
+    });
+    setShowDropdown(false);
+  }*/
+
+  //change to rotation
+
+  // async function handleSelectTask() {
+  //  assignNextTask();
+
+
+
+  //clicking task row
   const handleTaskPress = (task) => {
     setSelectedTask(task);
     task.mine ? setActiveView('MODAL') : setActiveView('VOTE');
@@ -48,7 +195,7 @@ export default function TasksScreen({ navigation }) {
         </TouchableOpacity>
       </View>
 
-      {/*refactor content*/}
+      {/* submit task screen*/}
       {activeView === 'SUBMIT' && (
         <SubmitTask
           task={selectedTask}
@@ -57,6 +204,7 @@ export default function TasksScreen({ navigation }) {
         />
       )}
 
+      {/* vote task screen*/}
       {activeView === 'VOTE' && (
         <VoteTask
           task={selectedTask}
@@ -65,6 +213,8 @@ export default function TasksScreen({ navigation }) {
         />
       )}
 
+
+      {/* task list*/}
       {activeView === 'LIST' && (
         <>
           <TaskFilter showMyTasks={showMyTasks} setShowMyTasks={setShowMyTasks} />
@@ -83,12 +233,41 @@ export default function TasksScreen({ navigation }) {
         </>
       )}
 
+      {/* my task modal window*/}
       <TaskModal
         visible={activeView === 'MODAL'}
         task={selectedTask}
         onClose={() => setActiveView('LIST')}
         onContinue={() => setActiveView('SUBMIT')}
       />
+
+      {/* dropdown menu*/}
+      {showDropdown && (
+        <View style={styles.dropdown}>
+          <ScrollView style={{ maxHeight: 200 }}>
+            {presetTasks.map((t) => (
+              <TouchableOpacity
+                key={t.id}
+                style={styles.dropdownItem}
+                onPress={() => assignNextTask(t.name)}
+              >
+                <Text style={styles.dropdownText}>{t.name}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      )}
+
+      {/* add task button*/}
+      {activeView === 'LIST' && (
+        <TouchableOpacity
+          style={styles.addTaskButton}
+          onPress={() => setShowDropdown(!showDropdown)}
+        >
+          <Text style={styles.addTaskText}>Add Task</Text>
+        </TouchableOpacity>
+      )}
+
     </View>
   );
 }
@@ -116,4 +295,45 @@ const styles = StyleSheet.create({
     height: 48,
     resizeMode: 'contain',
   },
+
+  dropdown: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginHorizontal: 20,
+    borderRadius: 10,
+    paddingVertical: 10,
+    marginBottom: 10,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+  },
+
+  dropdownItem: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+  },
+
+  dropdownText: {
+    fontSize: 18,
+  },
+
+  addTaskButton: {
+    backgroundColor: '#556DC2',
+    paddingVertical: 15,
+    borderRadius: 28,
+    width: '50%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginBottom: 30,
+    marginTop: 10,
+  },
+
+  addTaskText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+
 });
